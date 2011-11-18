d56611805a0f101a5feb9a4b05d44f1d0a867a2a
vnavel <valdounet@gmail.com>
2011-11-18 16:44:32 +0000

    Correction round.a (mauvais comportement avec les data.frame) Export de Xair2R Ajustement seuil alerte NO2

--- R/round.a.R
+++ R/round.a.R
@@ -1,6 +1,10 @@
 round.a <- function(x, digits=0){
-	x <- x * (10**digits)
-	ifelse (sapply (x%%1, function(x) isTRUE ( all.equal (x, 0.5) ) ),
-		ceiling(x)/(10**digits),
-		round(x)/(10**digits) )
+	.local <- function(x, digits) {
+		x <- x * (10**digits)
+		ifelse (sapply (x%%1, function(x) isTRUE ( all.equal (x, 0.5) ) ),
+			ceiling(x)/(10**digits),
+			round(x)/(10**digits) )
 	}
+	if (is.data.frame (x)) return (data.frame (lapply (x, .local, digits) ) )
+	.local (x, digits)
+}
