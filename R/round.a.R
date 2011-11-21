8301c847bb8976e03d3cfc6ccc6a6d2cd7d26417
vnavel <valdounet@gmail.com>
2011-11-21 12:48:26 +0000

    Correction et optimisation de la fonction round.a

--- R/round.a.R
+++ R/round.a.R
@@ -1,10 +1,11 @@
-round.a <- function(x, digits=0){
+round.a <- function(x, digits=0) {
 	.local <- function(x, digits) {
 		x <- x * (10**digits)
-		ifelse (sapply (x%%1, function(x) isTRUE ( all.equal (x, 0.5) ) ),
+		ifelse (abs (x%%1 - 0.5) < .Machine$double.eps^0.5,
 			ceiling(x)/(10**digits),
 			round(x)/(10**digits) )
 	}
 	if (is.data.frame (x)) return (data.frame (lapply (x, .local, digits) ) )
 	.local (x, digits)
 }
+
