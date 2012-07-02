64ab30e084e7d24cc069e555e22be88fc68132bd
Vladislav Navel <valdounet@gmail.com>
2012-07-02 12:33:57 +0200

    Correction de la fonction Xair2R pour qu'elle fonctionne avec RODBC (création d'une méthode dbDisconnect,RODBC)

--- R/dbListFields.R
+++ R/dbListFields.R
@@ -7,3 +7,11 @@ dbListFields <- function(conn, name, ...) UseMethod('dbListFields')
 #' @rdname dbListFields
 setMethod('dbListFields', signature ('RODBC', 'character'),
 	  function(conn, name, ...) unique(sqlColumns(conn, name)$COLUMN_NAME))
+
+#' @rdname dbListFields
+#' @aliases dbDisconnect
+#' @aliases dbDisconnect,RODBC-method
+dbDisconnect <- function(conn, ...) UseMethod('dbListFields')
+#' @rdname dbListFields
+setMethod('dbDisconnect', signature ('RODBC'),
+	  function(conn, ...) odbcClose( conn ))
