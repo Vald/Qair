d355813f0ab0bc105f97184f2210a22062af415b
Vladislav Navel <valdounet@gmail.com>
2012-10-01 16:44:29 +0200

    modification des tests pour alléger le paquet

--- tests/xrConnection.R
+++ tests/xrConnection.R
@@ -22,156 +22,33 @@ xrGetMethodesPrelevement(xr)
 
 xrGetCampagnes(xr, 'LARO', end='2011-01-01')
 
-xrGetReseaux(xr, 'R_SO2')
+# xrGetReseaux(xr, 'R_SO2')
 
 colonnes <- c(63, 64, 70, 92)
 xrGetStations(xr, 'Chas')[-colonnes]
-xrGetStations(xr, mesures='SO2')[-colonnes]
-xrGetStations(xr, reseaux='R_SO2')[-colonnes]
 xrGetStations(xr, campagnes='ANG12')[-colonnes]
 xrGetStations(xr, campagnes='ANG12', mesures='24')[-colonnes]
-xrGetStations(xr, campagnes='ANG12', mesures='SO2', collapse='OR')[-colonnes]
 
 summary(xrGetSitesPrelevement(xr, 'TLAROC_L'))
 xrGetSitesPrelevement(xr, campagnes='TLAROC2009')
 
-# Test de la fonction xrGetMesures
-#---------------------------------
+#==========================================
+# tests sur la fonction xrGetContinuousData
 
-# les arguments suivant vont être combinés pour vérifier la compatibilité
-# entre leurs usages
+xrGetContinuousData(xr, 'N2_VER', '2012-09-30', '2012-10-01')
+xrGetContinuousData(xr, 'N2_VER', '2012-09-30', '2012-10-01', tz='UTC')
+xrGetContinuousData(xr, 'N2_VER', '2012-09-30', '2012-10-01', tz='CET')
+xrGetContinuousData(xr, 'N2_VER', '2012-09-30', '2012-10-01', cursor=1)
+xrGetContinuousData(xr, 'N2_VER', '2012-09-30', '2012-10-01', tz='UTC', cursor=1)
+xrGetContinuousData(xr, 'N2_VER', '2012-09-30', '2012-10-01', tz='CET', cursor=1)
 
-arguments <- list (
-		list	(campagnes = 'ANG12',
-			 stations = 'ANG_14',
-			 polluants = '24'),
-		list 	(pattern = 'VER',
-			 reseaux = 'INDICE',
-			 polluants = '03'))
+#======================================
+# tests sur la fonction xrGetManualData
 
-dbDisconnect(xr)
-for (args in arguments)
-{
-	xr <- xrConnect()
-
-	# determination de toutes les combinaisons possibles d'arguments
-
-	tmp <- unlist( lapply (FUN = combn,
-			1:length(args),
-			x=args, simplify=FALSE),
-	       recursive=FALSE)
-
-	# on restreint les tests aux listes d'arguments composés de 2 éléments 
-	# minimum (ça évite de demander tous les PM10 qui évoluent sans cesse)
-	
-	tmp <- tmp[sapply(tmp, length) > 1]
-
-	# ajout de la connection à XR dans chacune des combinaisons d'arugments
-
-	for (i in 1:length(tmp) ) {
-		tmp[[i]]$conn <- xr
-	}
-
-	# tests
-
-	print( lapply(lapply (FUN = do.call, tmp, what=xrGetMesures), function(x) summary(x[setdiff(names(x), c('NO_APPAREIL', 'TYPE_RUE'))])))
-
-	dbDisconnect(xr)
-}
-rm(args, tmp)
-xr <- xrConnect()
-
-# test de la fonction xrGetContinuousData
-#----------------------------------------
-
-# les arguments suivant vont être combinés pour vérifier la compatibilité
-# entre leurs usages
-
-arguments <- list (
-		list	(campagnes = 'ANG12',
-			 stations = '14',
-			 polluants = '24'),
-		list 	(pattern = 'VER',
-			 reseaux = 'INDICE',
-			 polluants = '03'))
-
-dbDisconnect(xr)
-for (args in arguments)
-{
-	xr <- xrConnect()
-
-	# determination de toutes les combinaisons possibles d'arguments
-
-	tmp <- unlist( lapply (FUN = combn,
-			1:length(args),
-			x=args, simplify=FALSE),
-	       recursive=FALSE)
-
-	# ajout de valeurs pour les arguments 'period', 'what', 'conn',
-	# 'start' et 'end'
-	
-	tmp <- mapply (SIMPLIFY=FALSE, c, tmp,
-		       period = rep( c('h', 'qh', 'd', 'm', 'y'),
-				     length.out=length(tmp)),
-		       what = rep( c('value', 'state', 'both'),
-				   length.out=length(tmp) ),
-		       start = '2010-12-06',
-		       end = rep(c('2011-03-05', '2011-01-18', '2011-09-24',
-				   '2012-02-05', '2012-02-05'),
-				 length.out=length(tmp)))
-
-	# ajout de la connection à XR dans chacune des combinaisons d'arugments
-
-	for (i in 1:length(tmp) ) {
-		tmp[[i]]$conn <- xr
-	}
-
-	# tests
-
-	print(lapply(lapply (FUN = do.call, tmp, what=xrGetContinuousData), summary))
-
-	dbDisconnect(xr)
-}
-rm(args, tmp)
-xr <- xrConnect()
-
-# test de la fonction xrGetManualData
-#------------------------------------
-
-# les arguments suivant vont être combinés pour vérifier la compatibilité
-# entre leurs usages
-
-arguments <-list(campagnes = 'LEROY_1009',
-		 sites = 'PLEROY_002',
-		 polluants = '87')
-
-dbDisconnect(xr)
-xr <- xrConnect()
-
-# determination de toutes les combinaisons possibles d'arguments
-
-tmp <- unlist( lapply (FUN = combn,
-		1:length(arguments),
-		x=arguments, simplify=FALSE),
-       recursive=FALSE)
-
-# ajout de valeurs pour les arguments 'period', 'what', 'conn',
-# 'start' et 'end'
-
-tmp <- mapply (SIMPLIFY=FALSE, c, tmp,
-	       what = rep( c('value', 'state', 'both'),
-			   length.out=length(tmp) ),
-	       MoreArgs=list(
-			     start = '2009-12-06',
-			     end = '2011-02-05'))
-for (i in 1:length(tmp) ) {
-	tmp[[i]]$conn <- xr
-}
-
-# tests
-
-print(lapply( lapply (FUN = do.call, tmp, what=xrGetManualData), summary))
-
-dbDisconnect(xr)
-rm(tmp)
+xrGetManualData(xr, '2011-01-01', '2012-01-01', 'PNIORT_001', 'C6H6')
+xrGetManualData(xr, '2011-01-01', '2012-01-01', 'PNIORT_001', 'C6H6', tz='UTC')
+xrGetManualData(xr, '2011-01-01', '2012-01-01', 'PNIORT_001', 'C6H6', tz='CET')
+xrGetManualData(xr, '2011-01-01', '2012-01-01', 'PNIORT_001', 'C6H6', cursor=0.5)
+xrGetManualData(xr, '2011-01-01', '2012-01-01', 'PNIORT_001', 'C6H6', tz='UTC', cursor=0.5)
+xrGetManualData(xr, '2011-01-01', '2012-01-01', 'PNIORT_001', 'C6H6', tz='CET', cursor=0.5)
 
