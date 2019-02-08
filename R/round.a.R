c2c80556e6fba9938dd15c9c37aa876f81f73544
Vladislav Navel <valdounet@gmail.com>
2019-02-08 12:24:14 +0100

    correction arrondi (round.a) des valeurs négatives

--- R/round.a.R
+++ R/round.a.R
@@ -13,8 +13,10 @@
 #' @seealso \code{\link[base]{round}}, \code{\link{quantile.a}}
 round.a <- function(x, digits=0) {
 	.local <- function(x, digits) {
+		n <- ifelse(x < 0, -1L, 1L)
+		x <- abs(x)
 		x <- x * (10**digits)
-		ifelse (abs (x%%1 - 0.5) < .Machine$double.eps^0.5,
+		n * ifelse (abs (x%%1 - 0.5) < .Machine$double.eps^0.5,
 			ceiling(x)/(10**digits),
 			round(x)/(10**digits) )
 	}
