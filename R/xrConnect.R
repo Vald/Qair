0d45cd0321e1d13b4107eca07b9b49829d713bab
Vladislav Navel <valdounet@gmail.com>
2019-07-17 12:09:56 +0200

    debut traitement authentification

--- R/xrConnect.R
+++ R/xrConnect.R
@@ -10,6 +10,11 @@
 #' Les arguments non-spécifiés à l'appel de la fonction et qui n'apparaissent pas
 #' dans les options seront interactivement demandés à l'utilisateur.
 #'
+#' Les arguments uid et pwd sont les identifiants classiques de connexion à XR.
+#' Ils sont requis pour l'authentification auprès de la base. Si jamais ils ne correspondent
+#' pas, la connexion à XR est limitée et l'accès à toutes les informations n'est
+#' pas possible. Le package Qair fonctionne alors en mode dégradé.
+#'
 #' @param host Adresse de l'hôte hébergeant la base de données.
 #'	optionnel : sera demandé si nécessaire.
 #' @param port Port de l'hôte hébergeant la base de données (8443 ou 8080).
@@ -17,6 +22,10 @@
 #' @param version Version de Qair avec laquelle doivent être compatibles les résultats
 #'  des requêtes : 2 -> avec Qair_2* (rétro-compatibilité), 3 sinon.
 #'	optionnel : sera demandé si nécessaire.
+#' @param uid Identifiant pour la connexion. Optionnel, sera demandé si nécessaire. Voir
+#'  détails.
+#' @param pwd Mot de passe pour la connexion. Optionnel, sera demandé si nécessaire. Voir
+#'  détails.
 #' @return Une connexion à la base XR (il s'agit en fait d'une liste avec 'host' et
 #'  'port'. Cela permet de gérer plusieurs 'connexion' simultanément et de mimer
 #'  le comportement de Qair2).
@@ -31,7 +40,7 @@
 #' xrConnect()
 #'}
 #' @export
-xrConnect <- function(host=NULL, port=NULL, version=NULL) {
+xrConnect <- function(host=NULL, port=NULL, version=NULL, uid=NULL, pwd=NULL) {
 	if(!is.null(host)) {
 		options(Xair.host=host)
 	} else if(is.null(getOption('Xair.host'))) {
@@ -59,6 +68,15 @@ xrConnect <- function(host=NULL, port=NULL, version=NULL) {
 	}
 	if(!options()[['Xair.version']] %in% 2:3)
 		stop("Les versions de Qair acceptées sont uniquement 2 ou 3")
+
+	if(!is.null(uid)) {
+		options(Xair.=host)
+	} else if(is.null(getOption('Xair.host'))) {
+		cat('hote hebergeant la base de donnees :\n')
+		options(Xair.host=scan(what='character', nlines=1))
+		cat('\n')
+	}
+
 
 	xr <- list(host   = options()[['Xair.host']],
 			   port   = options()[['Xair.port']],
